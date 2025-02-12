b11d9e2bc8be2b91ade9ad8ec9f1a6e4a4ca53ec
dennisko <dennis0324@naver.com>
2025-02-12 23:42:25 +0900

    change dashboard to snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,38 @@
+local logo = [[
+        ⟋|､
+          (°､ ｡ 7
+          |､  ~ヽ
+            じしf_,)〳
+]]
+logo = string.rep("\n", 8) .. logo .. "\n\n"
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[
+    ⟋|､
+      (°､ ｡ 7
+      |､  ~ヽ
+        じしf_,)〳
+]],
+       -- stylua: ignore
+       ---@type snacks.dashboard.Item[]
+       keys = {
+         { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+         { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+         { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+         { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+         { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+         { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+         { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+       },
+        },
+      },
+    },
+  },
+}
