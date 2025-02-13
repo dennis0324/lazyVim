0e9dbbe2bf76ff722ae6fb16c277cfc7c23990e5
donghyunKo-define <define.kdh@gmail.com>
2025-02-13 14:54:14 +0900

    refactor: change nvim-cmp to snacks

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,8 +1,19 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/vim-vsnip" },
     opts = function(_, opts)
       local cmp = require("cmp")
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
       opts.mapping = cmp.mapping.preset.insert({
         -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -10,7 +21,20 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         }),
-        -- ["<C-e>"] = cmp.mapping.close(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            local entries = cmp.get_entries()
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+
+            if #entries == 1 then
+              cmp.confirm()
+            end
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
         -- ["<Tab>"] = cmp.mapping.confirm({
         --   behavior = cmp.ConfirmBehavior.Replace,
         --   select = true,
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,6 +9,100 @@ return {
   {
     "snacks.nvim",
     opts = {
+      picker = {
+        -- your picker configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        --    -- Top Pickers & Explorer
+        keys = {
+
+          {
+            "<leader><space>",
+            function()
+              Snacks.picker.smart()
+            end,
+            desc = "Smart Find Files",
+          },
+          {
+            "<leader>,",
+            function()
+              Snacks.picker.buffers()
+            end,
+            desc = "Buffers",
+          },
+          {
+            "<leader>/",
+            function()
+              Snacks.picker.grep()
+            end,
+            desc = "Grep",
+          },
+          {
+            "<leader>:",
+            function()
+              Snacks.picker.command_history()
+            end,
+            desc = "Command History",
+          },
+          {
+            "<leader>n",
+            function()
+              Snacks.picker.notifications()
+            end,
+            desc = "Notification History",
+          },
+          {
+            "<leader>e",
+            function()
+              Snacks.explorer()
+            end,
+            desc = "File Explorer",
+          },
+          -- find
+          {
+            "<leader>fb",
+            function()
+              Snacks.picker.buffers()
+            end,
+            desc = "Buffers",
+          },
+          {
+            "<leader>fc",
+            function()
+              Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+            end,
+            desc = "Find Config File",
+          },
+          {
+            "<leader>ff",
+            function()
+              Snacks.picker.files()
+            end,
+            desc = "Find Files",
+          },
+          {
+            "<leader>fg",
+            function()
+              Snacks.picker.git_files()
+            end,
+            desc = "Find Git Files",
+          },
+          {
+            "<leader>fp",
+            function()
+              Snacks.picker.projects()
+            end,
+            desc = "Projects",
+          },
+          {
+            "<leader>fr",
+            function()
+              Snacks.picker.recent()
+            end,
+            desc = "Recent",
+          },
+        },
+      },
       dashboard = {
         preset = {
           pick = function(cmd, opts)
