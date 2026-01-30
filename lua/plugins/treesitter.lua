d2d0e71e5f9d8415fd198ff652186764e063386a
dennisko <dennis0324@naver.com>
2026-01-31 01:42:01 +0900

    refactor: update tresitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,29 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.highlight.enable = true
-      opts.ensure_installed = {
-        "astro",
-        "cmake",
-        "cpp",
-        "css",
-        "fish",
-        "gitignore",
-        "go",
-        "graphql",
-        "http",
-        "php",
-        "rust",
-        "scss",
-        "sql",
-        "svelte",
-        "javascript",
-        "typescript",
-        "tsx",
-      }
-    end,
+    lazy = false,
+    build = ":TSUpdate",
+    -- opts = function(_, opts)
+    --   opts.highlight.enable = true
+    --   opts.ensure_installed = {
+    --     "astro",
+    --     "cmake",
+    --     "cpp",
+    --     "css",
+    --     "fish",
+    --     "gitignore",
+    --     "go",
+    --     "graphql",
+    --     "http",
+    --     "php",
+    --     "rust",
+    --     "scss",
+    --     "sql",
+    --     "svelte",
+    --     "javascript",
+    --     "typescript",
+    --     "tsx",
+    --   }
+    -- end,
   },
 }
